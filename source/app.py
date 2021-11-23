71c369c15519891a4184f3f62e8022b39f831b02
PARKHARAM <33815176+PARKHARAM@users.noreply.github.com>
2021-11-23 17:58:59 +0900

    Update app.py

--- source/app.py
+++ source/app.py
@@ -9,7 +9,7 @@ def index():
   return render_template('index.html')
 
 if __name__=="__main__":
-   app.run(host="34.64.224.99", port="80", debug=True)
+   app.run(host="34.64.224.99", port="5000", debug=True)
   #app.run(debug=True)
   # host 등을 직접 지정하고 싶다면
   # app.run(host="127.0.0.1", port="5000", debug=True)
