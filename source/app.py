3d66960ab6a3dce4efeef0fe829666a6829ea3ff
PARKHARAM <33815176+PARKHARAM@users.noreply.github.com>
2021-11-24 09:17:38 +0900

    Update app.py

--- source/app.py
+++ source/app.py
@@ -10,7 +10,7 @@ def index():
 
 if __name__=="__main__":
   #app.run(host="34.64.224.99", port="80", debug=True)
-  app.run(host='0.0.0.0', port="5000", debug=True)
-  #app.run(debug=True)
+  #app.run(host='0.0.0.0', port="5000", debug=True)
+  app.run(debug=True)
   # host 등을 직접 지정하고 싶다면
   # app.run(host="127.0.0.1", port="5000", debug=True)
